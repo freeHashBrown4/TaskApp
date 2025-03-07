d5c1510f052e3b85e65b284fa93c9f7e44132f75
Muhammad Choudhary <choudha1@sheridancollege.ca>
2025-03-07 09:17:49 -0500

    Initial Commit

---
+++ TaskApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskApp
+//
+//  Created by Muhammad Choudhary on 2025-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskApp/TaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by Muhammad Choudhary on 2025-03-07.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
